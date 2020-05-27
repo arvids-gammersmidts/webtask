cf2bb554f24e52b1fdb4dff690092834be1d68e5
Kristers Dzintars <faceree123@gmail.com>
2020-05-27 14:31:47 +0300

    added list and grid view for products and add product button

---
+++ script/script.js
@@ -0,0 +1,28 @@
+$(function() {
+    var $radioBtn = $("input:radio");
+
+    $radioBtn.change(function() {
+        $radioBtn.parent().removeClass("checked");
+        $(this).parent().addClass("checked");
+    });
+    
+    $("#view").change(function() {
+        if ( $("#view option:selected").val() == "list" ) {
+            $(".product-container").removeClass("grid");
+            $(".product-container").addClass("list");
+        } else {
+            $(".product-container").removeClass("list");
+            $(".product-container").addClass("grid");
+        }
+    });
+    
+    var $checkbox = $("input:checkbox");
+    $checkbox.change(function() {
+        if( this.checked ) {
+            $(this).parent().addClass("checked");
+        } else {
+            $(this).parent().removeClass("checked");
+        }
+
+    });
+});
