c8f32f598eefc4505fa331d06665472bf91c4c52
Kristers Dzintars <faceree123@gmail.com>
2020-05-27 17:58:46 +0300

    updated jquery script

--- script/script.js
+++ script/script.js
@@ -1,13 +1,13 @@
-$(function() {
+$(function () {
     var $radioBtn = $("input:radio");
 
-    $radioBtn.change(function() {
+    $radioBtn.change(function () {
         $radioBtn.parent().removeClass("checked");
         $(this).parent().addClass("checked");
     });
-    
-    $("#view").change(function() {
-        if ( $("#view option:selected").val() == "list" ) {
+
+    $("#view").change(function () {
+        if ($("#view option:selected").val() == "list") {
             $(".product-container").removeClass("grid");
             $(".product-container").addClass("list");
         } else {
@@ -15,14 +15,13 @@ $(function() {
             $(".product-container").addClass("grid");
         }
     });
-    
+
     var $checkbox = $("input:checkbox");
-    $checkbox.change(function() {
-        if( this.checked ) {
+    $checkbox.change(function () {
+        if (this.checked) {
             $(this).parent().addClass("checked");
         } else {
             $(this).parent().removeClass("checked");
         }
-
     });
 });
